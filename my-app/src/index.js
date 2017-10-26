96bdec8d144a743141988ec1a08180470f9176d6
Justin Baize <justin.baize@gJBc897ad.local>
2017-10-26 12:02:22 -0500

    Established first form that will console.log inputs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,27 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Page extends React.Component {
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="">
-        <div className="">
-          first Name
-        </div>
-        <div className="">
-          last Name
-        </div>
-        <div className="">
-          <div></div>
-        </div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            first name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {/* <form onSubmit={this.handleSubmit}>
+          <label>
+            last name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form> */}
       </div>
     );
   }
 }
 
-// ========================================
+// class Page extends React.Component {
+//   render() {
+//     return (
+//       <div className="">
+//         <form onSubmit={this.handeSubmit}>
+//           <label>
+//             first name:
+//             <input type="text"
+//                    value={this.state.value}
+//                    onChange={this.handleChange}/>
+//           </label>
+//           <input type="submit" name="Submit"/>
+//         </form>
+//         <form>
+//           <label>last name
+//             <input type="text" name="lastName"/>
+//           </label>
+//           <input type="submit" name="Submit"/>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(
-  <Page />,
+  <NameForm />,
   document.getElementById('root')
 );

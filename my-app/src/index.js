4c49ff6c20675f46678ad6430754543e854b1923
Justin Baize <justin.baize@gJBc897ad.local>
2017-10-27 12:04:24 -0500

    Hitting the submit button will log to the console.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,48 +4,86 @@ import './index.css';
 
 import { Form, Text } from 'react-form';
 
- class BasicForm extends React.Component {
-    constructor( props ) {
-      super( props );
-      this.state = {};
-    }
+class BasicForm extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      userEmail:"",
+      userFirstName:"",
+      userLastName:"",
+      opponentEmail:"",
+      opponentFirstName:"",
+      opponentLastName:""
+    };
 
-    render() {
-      return (
-        <div>
-          <Form onSubmit={submittedValues => this.setState( { submittedValues } )}>
-            { formApi => (
-              <form onSubmit={formApi.submitForm} id="form2">
-                <label htmlFor="userEmail">Email</label>
-                <Text field="userEmail" id="userEmail" />
-                <br/>
-                <label htmlFor="userFirstName">First Name</label>
-                <Text field="userFirstName" id="userFirstName" />
-                <br/>
-                <label htmlFor="userLastName">Last Name</label>
-                <Text field="userLastName" id="userLastName" />
-                <br/>
-                <label htmlFor="opponentEmail">Opponent Email</label>
-                <Text field="opponentEmail" id="opponentEmail" />
-                <br/>
-                <label htmlFor="opponentFirstName">Opponent First Name</label>
-                <Text field="opponentFirstName" id="opponentFirstName" />
-                <br/>
-                <label htmlFor="opponentLastName">Opponent Last Name</label>
-                <Text field="opponentLastName" id="opponentLastName" />
-                <br/>
-                <button type="submit" className="mb-4 btn btn-primary">Submit</button>
-              </form>
-            )}
-          </Form>
-        </div>
-      );
-    }
+    this.setStateOnSubmit = this.setStateOnSubmit.bind(this);
   }
+
+  setStateOnSubmit(state){
+    console.log("setStateOnSubmit invoked with param 'state'>>>",state);
+
+    //NOTE: this works but is hideous!
+    let userEmail = state.userEmail ? state.userEmail : '';
+    let userFirstName = state.userFirstName ? state.userFirstName : '';
+    let userLastName = state.userLastName ? state.userLastName : '';
+    let opponentEmail = state.opponentEmail ? state.opponentEmail : '';
+    let opponentFirstName = state.opponentFirstName ? state.opponentFirstName : '';
+    let opponentLastName = state.opponentLastName ? state.opponentLastName : '';
+
+    //NOTE: WHY DOESN'T THIS WORK? --> this.setState({state:state})
+    this.setState(
+      {
+        userEmail:userEmail,
+        userFirstName:userFirstName,
+        userLastName:userLastName,
+        opponentEmail:opponentEmail,
+        opponentFirstName:opponentFirstName,
+        opponentLastName:opponentLastName,
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={submittedValues => this.setStateOnSubmit(submittedValues)}>
+          { formApi => (
+            <form onSubmit={formApi.submitForm} id="form2">
+              <label htmlFor="userEmail">Email</label>
+              <br/>
+              <Text field="userEmail" id="userEmail" />
+              <br/>
+              <label htmlFor="userFirstName">First Name</label>
+              <br/>
+              <Text field="userFirstName" id="userFirstName" />
+              <br/>
+              <label htmlFor="userLastName">Last Name</label>
+              <br/>
+              <Text field="userLastName" id="userLastName" />
+              <br/>
+              <label htmlFor="opponentEmail">Opponent Email</label>
+              <br/>
+              <Text field="opponentEmail" id="opponentEmail" />
+              <br/>
+              <label htmlFor="opponentFirstName">Opponent First Name</label>
+              <br/>
+              <Text field="opponentFirstName" id="opponentFirstName" />
+              <br/>
+              <label htmlFor="opponentLastName">Opponent Last Name</label>
+              <br/>
+              <Text field="opponentLastName" id="opponentLastName" />
+              <br/>
+              <button type="submit" className="mb-4 btn btn-primary">Submit</button>
+            </form>
+          )}
+        </Form>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <div>
-    {/* <NameForm /> */}
     <BasicForm/>
   </div>,
   document.getElementById('root')
